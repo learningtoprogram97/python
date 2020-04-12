cfc2d4b194a9685873c1628348dbf40e1ace77f5
abarton97 <62631343+abarton97@users.noreply.github.com>
2020-04-12 14:08:35 -0700

    file_organizer.py update 4/12/2020 Second Hour

--- austin/file_organizer.py
+++ austin/file_organizer.py
@@ -1,40 +1,62 @@
 import os
+import sys
 
 
-class bcolors:
+class Colors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
+    GREEN = '\033[92m'
     YELLOW = '\033[93m'
-    FAIL = '\033[91m'
+    RED = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
 
-def main():
-    photos()
+folder_location = input("Please enter your folder location: ")
 
 
 def photos():
     # example C:/Users/Austin/Documents
-    folder_location = input("Please enter your folder location: ")
     # check file types in folder_location
+    """
+    ###
+    #start photo organization
+    ###
+    """
     photo_file_extensions = ("jpg", "jpeg", "png", "bmp", "gif")
     # prints list of photos
     dir_name = folder_location + '/photos'
     try:
         # Create target Directory
         os.mkdir(dir_name)
-        print("Directory ", dir_name, " Created ")
+        print(f"{Colors.HEADER}Directory{Colors.ENDC} {dir_name} {Colors.HEADER}Created{Colors.ENDC} \n")
     except FileExistsError:
         # checks if folder exists
-        print("Directory ", dir_name, " already exists")
+        print(f"{Colors.RED}Directory {Colors.ENDC}{dir_name} {Colors.RED}already exists{Colors.ENDC}\n")
     for file in os.listdir(folder_location):
         if file.endswith(photo_file_extensions):
             # prints all file types
             # Create directory
-            print(f"{bcolors.YELLOW}File Name:{bcolors.ENDC} {file}")
+            print(f"{Colors.YELLOW}File Name:{Colors.ENDC} {file}")
+    documents()
 
 
-main()
+def documents():
+    dir_name = folder_location + '/documents'
+    document_file_extensions = ("doc", "pdf", "txt", "ods", "XLS")
+    try:
+        # Create target Directory
+        os.mkdir(dir_name)
+        print(f"{Colors.HEADER}Directory{Colors.ENDC} {dir_name} {Colors.HEADER}Created{Colors.ENDC} \n")
+    except FileExistsError:
+        # checks if folder exists
+        print(f"{Colors.RED}Directory {Colors.ENDC}{dir_name} {Colors.RED}already exists{Colors.ENDC}\n")
+    for file in os.listdir(folder_location):
+        if file.endswith(document_file_extensions):
+            # prints all file types
+            # Create directory
+            print(f"{Colors.OKBLUE}File Name:{Colors.ENDC} {file}")
+
+
+photos()
